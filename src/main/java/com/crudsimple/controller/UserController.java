927508d53e49f8f700bc379435f8b9ee65e6b017
GDN\aji.pramono <aji.pramono@gdn-commerce.com>
2021-08-16 02:18:07 +0700

    Added API for get all users

--- src/main/java/com/crudsimple/controller/UserController.java
+++ src/main/java/com/crudsimple/controller/UserController.java
@@ -5,10 +5,9 @@ import com.crudsimple.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * The UserController Class is a Rest Controller class which will have all end points for User
@@ -36,4 +35,16 @@ public class UserController {
         User createdUser = userService.saveUser(user);
         return new ResponseEntity<Object>(createdUser, HttpStatus.CREATED);
     }
+
+    /**
+     * Get all list of users based on GET request.
+     * @return
+     */
+
+    @GetMapping(value = "/user")
+    public ResponseEntity<?> getAllUsers() {
+        List<User> userList = userService.findAllUsers();
+        return new ResponseEntity<Object>(userList, HttpStatus.OK);
+    }
+    
 }
